3b935f2a0b005de5eb745d27e1b7bfcbd92b4f07
Yohan Chatelain <yohan.chatelain@gmail.com>
2022-11-08 18:01:15 -0500

    Fix package name

--- significantdigits/conftest.py
+++ significantdigits/conftest.py
@@ -37,8 +37,8 @@ class RunMetricDigitsTest:
     def __init__(self, metric):
         self.metric = self.check_metric(metric)
         self.bases = [2, 10]
-        self.methods = significantdigits.significantdigits.Method
-        self.errors = significantdigits.significantdigits.Error
+        self.methods = significantdigits.Method
+        self.errors = significantdigits.Error
 
     def check_metric(self, metric):
         if metric in self.available_metrics:
@@ -49,11 +49,11 @@ class RunMetricDigitsTest:
             sys.exit(1)
 
     def compute_significant_digits(self, x, ref, error, method, base):
-        return significantdigits.significantdigits.significant_digits(
+        return significantdigits.significant_digits(
             x, ref, error=error, method=method, base=base)
 
     def compute_contributing_digits(self, x, ref, error, method, base):
-        return significantdigits.significantdigits.contributing_digits(
+        return significantdigits.contributing_digits(
             x, ref, error=error, method=method, base=base)
 
     def compute_metric(self, *args, **kwargs):
